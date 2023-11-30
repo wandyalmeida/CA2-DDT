ad3b3afd76d1f360e0c673b9c0fc5229b6d932e5
wandyalmeida <77290358+wandyalmeida@users.noreply.github.com>
2023-11-30 14:22:57 +0000

    Add some constructor and variables

--- contracts/Dentalappontiment.sol
+++ contracts/Dentalappontiment.sol
@@ -1,5 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+contract DentalAppointment {
+    struct Appointment {
+        address patient;
+        uint256 date;
+        bool isConfirmed;
+    }
 
+    mapping(uint256 => Appointment) public appointments;
+    address public dentist;
+
+    constructor() {
+        dentist = msg.sender;
+    }
 
 }
